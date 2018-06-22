7810e8be17fea22bfb7b69937b19770f8f575f1b
Stéphane Maniaci <stephane.maniaci@asos.com>
2018-06-22 11:34:19 +0100

    fix: better exports

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-const Enzyme = require("enzyme");
-const Adapter = require("enzyme-adapter-react-16");
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-const renderWithDefaultProps = require("./src/renderWithDefaultProps");
-const testReduxComponent = require("./src/testReduxComponent.js");
-
-module.exports = {
-  renderWithDefaultProps,
-  testReduxComponent
-};
+export { renderWithDefaultProps } from "./src/renderWithDefaultProps";
+export { testReduxComponent } from "./src/testReduxComponent.js";
